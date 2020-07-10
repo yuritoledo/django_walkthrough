2ef42de3076a9195f3cd2fca1fb0e8b0d3578f1e
Yuri Toledo <yuriwtoledo@gmail.com>
2020-07-10 07:22:27 -0300

    adding readme and dev stuff

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello buddy!')
+    return HttpResponse("Hello buddy!")
 
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}')
+    return HttpResponse(f"You are looking at question {question_id}")
 
 
 def results(request, question_id):
